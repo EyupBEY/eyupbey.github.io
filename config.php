bc28d0270045e8c3179135b07796272cb1c24f64
EyupBEY <66494001+EyupBEY@users.noreply.github.com>
2021-06-20 12:59:36 +0300

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host="aws-www-ceyda-live.cnox2nsxjmiv.eu-central-1.rds.amazonaws.com"; // Host name 
-$username="EyupBEY"; // Mysql username 
-$password="somePass1234+"; // Mysql password
-$db_name="letter_for_the_special_one"; // Database name
-$conn=mysqli_connect($host,$username,$password,$db_name) or die ("Failed to connect with DB");
-$base_url ='https://www.atalet.me'; //plz change this url
-?>
